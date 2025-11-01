16995f950ef36e78ca60d4d2efc06fafc3115bf6
KingOfPlay <king200813top@gmail.com>
2025-11-01 15:58:45 +0300

    Сделал пз

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,2 +1,11 @@
-print('Hello world');
-print()
+# Дано двухзначноечное число. Вывести число, полученное при перестановке цифр исходного числа
+try:
+    a = int(input('Введите двухзначное число: '));
+    if 10 <= a <= 99:
+        tens = a // 10
+        units = a % 10
+        result = units * 10 + tens
+        print(f'Результат: {result}')
+    else:
+        print('Не двухзначное число')
+except ValueError: print("Не то")
