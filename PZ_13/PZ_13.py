8dd0fdf4e58783c76798e923108532d44f6d21e2
king2 <king200813top@gmaul.com>
2026-06-10 00:15:23 +0300

    ПЗ готово

--- PZ_13/PZ_13.py
+++ PZ_13/PZ_13.py
@@ -4,32 +4,23 @@
 
 import re
 
-# Название исходного и результирующего файлов
 INPUT_FILE = "dates1.txt"
 OUTPUT_FILE = "february_dates.txt"
 
-# Регулярное выражение для поиска дат в форматах ДД.ММ.ГГГГ или ДД/ММ/ГГГГ
-# \d{2} - две цифры дня, [./] - разделитель точка или слэш,
-# \d{2} - две цифры месяца, \d{4} - четыре цифры года
 DATE_PATTERN = r"\b(\d{2})[./](\d{2})[./](\d{4})\b"
 
 
 def process_dates():
     try:
-        # Чтение данных из исходного файла
         with open(INPUT_FILE, "r", encoding="utf-8") as file:
             content = file.read()
 
-        # Поиск всех дат. findall вернет список кортежей вида ('ДД', 'ММ', 'ГГГГ')
         all_dates = re.findall(DATE_PATTERN, content)
 
-        # Фильтрация и форматирование дат за февраль ('02') с помощью спискового включения
-        # Формируем строку в требуемом формате ДД/ММ/ГГГГ
         february_dates = [
             f"{day}/02/{year}" for day, month, year in all_dates if month == "02"
         ]
 
-        # Запись отфильтрованных дат в новый текстовый файл
         with open(OUTPUT_FILE, "w", encoding="utf-8") as out_file:
             for date_str in february_dates:
                 out_file.write(date_str + "\n")
